9ea48a8d2075d43a40f7f465982633fcdfe72371
khutaija <shariff.k@husky.neu.edu>
2016-11-17 18:09:44 -0800

    Week two code

---
+++ Sana_WeekTwo/js/myJavascript.js
@@ -0,0 +1,37 @@
+/**
+ * Created by khutaijashariff on 11/8/16.
+ */
+
+$('.carousel').carousel({
+    interval: 5000 //changes the speed
+})
+
+
+function openPage2() {
+    window.location = 'http://webproject-team4-1.s3-website-us-east-1.amazonaws.com/#';
+}
+
+function showText1() {
+    $('#img1').removeClass('hidden');
+}
+function showText2() {
+    $('#img2').removeClass('hidden');
+}
+function showText3() {
+    $('#img3').removeClass('hidden');
+}
+
+function hideText1() {
+    $('#img1').addClass('hidden');
+}
+function hideText2() {
+    $('#img2').addClass('hidden');
+}
+function hideText3() {
+    $('#img3').addClass('hidden');
+}
+
+$(function () {
+    $("#sortable").sortable();
+    $("#sortable").disableSelection();
+});
